02d5e432260184db05d72d1750cc808e5d9ea1ab
Alexis Hachemi <alexishachemi@gmail.com>
2025-01-07 21:14:53 +0100

    Add expression casting

--- lib/Interpreter/System/Types.hs
+++ lib/Interpreter/System/Types.hs
@@ -1,45 +1,45 @@
 module Interpreter.System.Types
     (
-        -- castExpr,
-        -- toLiteralExpr
+        castExpr,
+        toLiteralExpr
     )
 where
 
--- import Utils.Data.Result
--- import Parser.Data.Ast
--- import Interpreter.Environment
+import Utils.Data.Result
+import Parser.Data.Ast
+import Interpreter.Data.Environment
 
--- wrap :: (Int, Int) -> Int -> Int
--- wrap (mn, mx) n | n >= mn && n <= mx = n
---                    | n > mx = wrap (mn, mx) (mn + (n - mx))
---                    | otherwise = wrap (mn, mx) (mx - (n + mn))
+wrap :: (Integer, Integer) -> Integer -> Integer
+wrap (mn, mx) n | n >= mn && n <= mx = n
+                   | n > mx = wrap (mn, mx) (mn + (n - mx))
+                   | otherwise = wrap (mn, mx) (mx - (n + mn))
 
--- castInt :: Int -> Primitive -> Int
--- castInt n I8 = wrap (-128, 127) n
--- castInt n I16 = wrap (-32768, 32767) n
--- castInt n I32 = wrap (-2147483648, 2147483647) n
--- castInt n I64 = wrap (-9223372036854775808, 9223372036854775807) n
--- castInt n U8 = wrap (0, 255) n
--- castInt n U16 = wrap (0, 65535) n
--- castInt n U32 = wrap (0, 4294967295) n
--- -- castInt n U64 = wrap (0, 18446744073709551615) n -- Parsing uses Int so U64 is not possible 
--- castInt n _ = n
+castInteger :: Integer -> Primitive -> Integer
+castInteger n I8 = wrap (-128, 127) n
+castInteger n I16 = wrap (-32768, 32767) n
+castInteger n I32 = wrap (-2147483648, 2147483647) n
+castInteger n I64 = wrap (-9223372036854775808, 9223372036854775807) n
+castInteger n U8 = wrap (0, 255) n
+castInteger n U16 = wrap (0, 65535) n
+castInteger n U32 = wrap (0, 4294967295) n
+castInteger n U64 = wrap (0, 18446744073709551615) n 
+castInteger n _ = n
 
--- castFloat :: Float -> Primitive -> Float
--- castFloat n _ = n -- Parsing uses Float so F64 is not possible
+castDouble :: Double -> Primitive -> Double
+castDouble n _ = n -- Parsing uses Float so F64 is not possible
 
--- castLiteral :: Literal -> Primitive -> Literal
--- castLiteral (IntLiteral n) t
---     | t `elem` [F32, F64] = FloatLiteral $ castFloat (fromIntegral n) t
---     | otherwise = IntLiteral $ castInt n t
--- castLiteral (FloatLiteral n) t
---     | t `elem` [F32, F64] = FloatLiteral $ castFloat n t
---     | otherwise = IntLiteral $ castInt (round n) t
+castLiteral :: Literal -> Primitive -> Literal
+castLiteral (IntLiteral n) t
+    | t `elem` [F32, F64] = FloatLiteral $ castDouble (fromIntegral n) t
+    | otherwise = IntLiteral $ castInteger n t
+castLiteral (FloatLiteral n) t
+    | t `elem` [F32, F64] = FloatLiteral $ castDouble n t
+    | otherwise = IntLiteral $ castInteger (round n) t
 
--- castExpr :: Env -> Expression -> Type -> Result String Expression
--- castExpr _ (ELiteral l) (PrimitiveType t) = Ok $ ELiteral $ castLiteral l t
--- castExpr _ _ t = Err $ "Failed to cast expression to " ++ show t
+castExpr :: Env -> Expression -> Type -> Result String Expression
+castExpr _ (ELiteral l) (PrimitiveType t) = Ok $ ELiteral $ castLiteral l t
+castExpr _ _ t = Err $ "Failed to cast expression to " ++ show t
 
--- toLiteralExpr :: Type -> Expression -> Result String Literal
--- toLiteralExpr _ (ELiteral v) = Ok v
--- toLiteralExpr _ v = Err $ "Cannot deduce literal from " ++ show v
+toLiteralExpr :: Type -> Expression -> Result String Literal
+toLiteralExpr _ (ELiteral v) = Ok v
+toLiteralExpr _ v = Err $ "Cannot deduce literal from " ++ show v
