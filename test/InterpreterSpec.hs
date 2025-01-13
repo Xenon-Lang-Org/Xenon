67d8e3cb7239d5a7f3d4ce7e9d22af03c74a70d0
Alexis Hachemi <alexishachemi@gmail.com>
2025-01-13 10:36:32 +0100

    Add eval expr tests

--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -30,9 +30,9 @@ spec = do
             fromEnv (envWithVar "foo") "bar" `shouldBe` Err "bar is undefined"
 
         it "should add an EnvVar to the Env" $ do
-            pushVariable (env True) (VariableDeclaration "foo" iType (Just $ iLit 0)) 
+            pushVariable (env True) (VariableDeclaration "foo" iI32 (Just $ iLit 0)) 
                 `shouldBe` Ok (envWithVar "foo")
-            pushFunction (env True) (FunctionDeclaration "bar" eFuncArgs iType eFuncBody)
+            pushFunction (env True) (FunctionDeclaration "bar" eFuncArgs iI32 eFuncBody)
                 `shouldBe` Ok (envWithFunc "bar")
             pushType (env True) (sType "baz")
                 `shouldBe` Ok (envWithType "baz")
@@ -41,25 +41,58 @@ spec = do
             assignVar (envWithVar "foo") "foo" (iLit 12) `shouldSatisfy` varHasValue "foo" (iLit 12)
             assignVar (envWithVar "foo") "bar" (iLit 12) `shouldSatisfy` isErr 
 
+
+        -- Types
+
+        it "should wrap an Integer according to a primitive" $ do
+            castExpr (env True) (iLit 255) iU8 `shouldSatisfy` isExpr (iLit 255)
+            castExpr (env True) (iLit 255) iI8 `shouldSatisfy` isExpr (iLit (-1))
+            castExpr (env True) (iLit 256) iU8 `shouldSatisfy` isExpr (iLit 0)
+            castExpr (env True) (iLit (-1)) iU8 `shouldSatisfy` isExpr (iLit 255)
+            castExpr (env True) (iLit (-1)) iI32 `shouldSatisfy` isExpr (iLit (-1))
+            castExpr (env True) (iLit (-1)) iU32 `shouldSatisfy` isExpr (iLit 4294967295)
+
+        it "should evaluate the expression" $ do
+
+            -- Binary Operation
+            evalExpr (envWith True [eVarI "foo" 12]) (BinaryOp Add (Variable "foo") (iLit 8))
+                `shouldSatisfy` mEvalExprIs (iLit 20)
+            evalExpr (envWith True [eVarI "foo" 0]) (BinaryOp Div (iLit 8) (Variable "foo"))
+                `shouldBe` Err "Division by zero"
+
+
+
 -- Helpers
 
 iLit :: Integer -> Expression
 iLit i = ELiteral $ IntLiteral i
 
-iType :: Type
-iType = PrimitiveType I32
+iI8 :: Type
+iI8 = PrimitiveType I8
+
+iI32 :: Type
+iI32 = PrimitiveType I32
+
+iU8 :: Type
+iU8 = PrimitiveType U8
+
+iU32 :: Type
+iU32 = PrimitiveType U32
 
 eVar :: String -> EnvVar
-eVar n = EVariable n iType (ELiteral $ IntLiteral 0)
+eVar n = EVariable n iI32 (ELiteral $ IntLiteral 0)
+
+eVarI :: String -> Integer -> EnvVar
+eVarI n i = EVariable n iI32 (iLit i)
 
 eFuncBody :: Body
 eFuncBody = [ReturnStatement $ BinaryOp Add (Variable "a") (Variable "b")]
 
 eFuncArgs :: [Field]
-eFuncArgs = [("a", iType), ("b", iType)]
+eFuncArgs = [("a", iI32), ("b", iI32)]
 
 eFunc :: String -> EnvVar
-eFunc n = EFunction n  eFuncArgs iType eFuncBody
+eFunc n = EFunction n  eFuncArgs iI32 eFuncBody
 
 tEnum :: String -> TypeDefinition
 tEnum n = EnumDeclaration n ["RED", "GREEN", "BLUE"]
@@ -88,3 +121,20 @@ varHasValue n v (Ok e) = case fromEnv e n of
     Ok (EVariable _ _ v') -> v == v'
     _ -> False
 varHasValue _ _ _ = False
+
+isExpr :: Expression -> Result String Expression -> Bool
+isExpr _ (Err _) = False
+isExpr v (Ok v') = v == v'
+
+mEvalExprIs :: Expression -> Result String (Env, Expression) -> Bool
+mEvalExprIs _ (Err _) = False
+mEvalExprIs v (Ok (_, v')) = v == v'
+
+mEvalEnvIs :: Env -> Result String (Env, Expression) -> Bool
+mEvalEnvIs _ (Err _) = False
+mEvalEnvIs e (Ok (e', _)) = e == e'
+
+envHasVar :: Env -> String -> Expression -> Bool
+envHasVar e n v = case fromEnv e n of
+    Ok (EVariable _ _ v') -> v == v'
+    _ -> False
